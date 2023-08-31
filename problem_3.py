f973636bae283a67d4dc00facf6ae3bf17569c9f
Brunahellen0 <142901267+Brunahellen0@users.noreply.github.com>
2023-08-31 11:36:17 -0300

    Update problem_3.py

--- problem_3.py
+++ problem_3.py
@@ -28,22 +28,13 @@ Os números são..: 12 13 17
 
 """
 
-def soma():
+from library_problem_3 import soma
 
-    while True:
-            x = int(input("Digite o valor inicial: "))
-            if x == 0:
-                break
+def main():
 
-            soma = 0
-            if x % 2 == 1:
-                x += 1
-
-            for i in range(x, x+10, 2):
-                soma += i
-
-            print(f"A soma dos 5 pares é..: {soma}")
-
+    print("=-=-=-=-=-=-=-=-=-=")
+    soma()
+    print("=-=-=-=-=-=-=-=-=-=")
 
 if __name__ == '__main__':
     main()
