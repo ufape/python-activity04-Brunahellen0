7d9697e5f7f31b614689f6d3b9ba4e955970c4df
Brunahellen0 <142901267+Brunahellen0@users.noreply.github.com>
2023-08-31 11:37:49 -0300

    Update problem_4.py

--- problem_4.py
+++ problem_4.py
@@ -29,25 +29,11 @@ Teste 3: 28
 28 é perfeito.
 """
 
-def resultado():
-    def e_perfeito(n):
-        soma = 0
-        for i in range(1, n):
-            if n % i == 0:
-                soma += i
-        return soma == n
+from library_problem_4 import resultado
 
-    def testar_numeros_perfeitos():
-        n = int(input("Digite quantos testes realizará: "))
-        for i in range(1, n+1):
-            x = int(input(f"Teste {i}: "))
-            if e_perfeito(x):
-                print(f"{x} é perfeito.")
-            else:
-                print(f"{x} não é perfeito.")
+def main():
 
-    testar_numeros_perfeitos()
-
+    resultado()
 
 
 if __name__ == '__main__':
