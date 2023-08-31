5e5a8f12506d0db1eff4d6ac2f067102ae7e749e
Brunahellen0 <142901267+Brunahellen0@users.noreply.github.com>
2023-08-31 11:13:26 -0300

    Update problem_2.py

--- problem_2.py
+++ problem_2.py
@@ -38,7 +38,7 @@ Os números são..: 12 13 17
         x, y = y, x
     numeros = []
     for i in range(x+1, y):
-        if i % 5 == 2 or i % 5 == 3:
+    if i % 5 == 2 or i % 5 == 3:
             numeros.append(i)
 
     print("Os números são..: {}".format(" ".join(str(n) for n in numeros)))
