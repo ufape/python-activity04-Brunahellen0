0a7b2dd345edfb1acf55c9b04beb31445274ba18
Brunahellen0 <142901267+Brunahellen0@users.noreply.github.com>
2023-08-31 11:39:19 -0300

    Update problem_5.py

--- problem_5.py
+++ problem_5.py
@@ -29,30 +29,10 @@ Teste 3: 7
 7 é primo.
 """
 
-def e_primo(num):
-    """
-    Verifica se um número é primo ou não.
-    :param num: o número a ser verificado
-    :return: True se o número for primo, False caso contrário
-    """
-    if num <= 1:
-        return False
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
+from library_problem_5 import teste
 
-
-def teste():
-    n = int(input("Digite quantos testes realizará: "))
-    for i in range(1, n+1):
-        x = int(input("Teste {}: ".format(i)))
-        if e_primo(x):
-            print("{} é primo.".format(x))
-        else:
-            print("{} não é primo.".format(x))
-
-
+def main():
+    teste()
 
 if __name__ == '__main__':
     main()
